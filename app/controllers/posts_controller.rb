60bc616602e3bdd83c16d92d51063cecd56e4e84
weteamsteve <weteamsteve@gmail.com>
2019-04-26 18:42:25 -0400

    Add specs for the Post model's validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,6 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    #if user_signed_in?
-    ##  @message_has_been_sent = conversation_exist?
-    #end
   end
 
   def new
@@ -37,10 +34,6 @@ class PostsController < ApplicationController
   end
 
   private
-
-  #def conversation_exist?
-  #  Private::Conversation.between_users(current_user.id, @post.user.id).present?
-  #end
 
   def post_params
     params.require(:post).permit(:content, :title, :category_id)
