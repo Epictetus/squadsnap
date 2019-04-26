6056958461a348dccfb2bfaa75cded0b3730722c
weteamsteve <weteamsteve@gmail.com>
2019-04-26 17:42:43 -0400

    Define a default_scope for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   # Associations
   belongs_to :user
   belongs_to :category
+
+  # Define scope for search, this orders posts in descending order by the creation date, newest parts will be at the top
+  default_scope -> { includes(:user).order(created_at: :desc) }
 end
