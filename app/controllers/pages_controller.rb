6a877ca6db2b3f9170d5f00d45831b13ec990999
weteamsteve <weteamsteve@gmail.com>
2019-05-02 21:15:35 -0400

    remove team from branches.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,5 @@ class PagesController < ApplicationController
   def index
     @hobby_posts = Post.by_branch('hobby').limit(8)
     @study_posts = Post.by_branch('study').limit(8)
-    @team_posts = Post.by_branch('team').limit(8)
   end
 end
