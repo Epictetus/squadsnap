af3fd0929bae5789fa5599d13baa9fd57b335e83
weteamsteve <weteamsteve@gmail.com>
2019-05-02 21:11:11 -0400

    remove team as a branch.

--- spec/features/posts/create_new_post_spec.rb
+++ spec/features/posts/create_new_post_spec.rb
@@ -19,5 +19,4 @@ RSpec.feature "Create a new post", :type => :feature do
 
   include_examples 'user creates a new post', 'hobby'
   include_examples 'user creates a new post', 'study'
-  include_examples 'user creates a new post', 'team'
 end
