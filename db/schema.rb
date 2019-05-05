9bae2c76c35d3eadbe03ccff86e5e2c8f6970d66
weteamsteve <weteamsteve@gmail.com>
2019-05-04 21:23:36 -0400

    💪 add_owner_to_squad migration, saves current_user.id as squad.owner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_224234) do
+ActiveRecord::Schema.define(version: 2019_05_05_004206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2019_05_04_224234) do
     t.string "sport"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "owner_id"
+    t.index ["owner_id"], name: "index_squads_on_owner_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -62,4 +64,5 @@ ActiveRecord::Schema.define(version: 2019_05_04_224234) do
   end
 
   add_foreign_key "members", "users"
+  add_foreign_key "squads", "users", column: "owner_id"
 end
