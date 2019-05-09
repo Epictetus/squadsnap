99bfb8da049f6aef39c4ab57b6d510299b364f69
weteamsteve <weteamsteve@gmail.com>
2019-05-09 18:35:01 -0400

    member approval in squads is working and implemented.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,15 +1,21 @@
 class MembersController < ApplicationController
   before_action :set_squad, only: [:approve, :reject]
-  before_action :set_user, only: [:approve, :reject]
+  before_action :set_member, only: [:approve, :reject]
   before_action :require_permission, only: [:approve, :reject]
 
   # PATCH/PUT /squads/1/approve/2
   # PATCH/PUT /squads/1/approve/2.json
-  def approve(member)
+  def approve()
     respond_to do |format|
-      member = Member.new(squad: @squad, user: @user, membership: 'member')
-      member.save(validate: false)
-      #format.html { redirect_to @squad, notice: "#{user.name} was approved." }
+      @member = @squad.members.find(params[:id])
+      @member.membership = 'member'
+      if @member.save
+        format.html { redirect_to @squad, notice: 'Member was approved to join the squad.' }
+        format.json { render :show, status: :ok, location: @squad }
+      else
+        format.html { render :edit }
+        format.json { render json: @squad.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -26,12 +32,12 @@ private
 
   # Use callbacks to share common setup or constraints between actions.
   def set_squad
-    @squad = Squad.find(params[:id])
+    @squad = Squad.find(params[:squad_id])
   end
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
+  def set_member
+    @member = Member.find(params[:id])
   end
 
   # Must be owner of a squad to edit or destroy it
