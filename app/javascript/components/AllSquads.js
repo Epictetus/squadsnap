239bf963da1223891d54887abc2460d912751486
weteamsteve <weteamsteve@gmail.com>
2019-05-15 00:39:07 -0400

    updated console.logs and buttons

--- app/javascript/components/AllSquads.js
+++ app/javascript/components/AllSquads.js
@@ -5,24 +5,29 @@ import Squad from './Squad'
 
 class AllSquads extends React.Component {
   handleDelete(id) {
-    console.log('allsquads handle delete')
+    console.log('AllSquads handleDelete (id: ' + id + ')')
     this.props.handleDelete(id);
   }
 
   onUpdate(squad) {
-      this.props.onUpdate(squad);
+    console.log('AllSquads onUpdate (squad: ' + squad + ')')
+    this.props.onUpdate(squad);
   }
 
   render () {
+    console.log('AllSquads render')
+
+    var context = this;
+
     var squads = this.props.squads.map((squad) => {
-            return (
-                <div key={squad.id}>
-                  <Squad squad={squad}
-                       handleDelete={this.handleDelete.bind(this, squad.id)}
-                       handleUpdate={this.onUpdate}/>
-                </div>
-            )
-        });
+        return (
+            <div key={squad.id}>
+              <Squad squad={squad}
+                   handleDelete={() => context.handleDelete.bind(this, squad.id)}
+                   handleUpdate={() => context.onUpdate}/>
+            </div>
+        )
+    });
 
     return (
       <React.Fragment>
