c0c1578223210b78688ef2aa79092a2825719f54
Steven Donald <weteamsteve@gmail.com>
2019-05-31 21:45:29 -0400

    Update readme and security vulnerabilities

--- app/javascript/components/Squad.js
+++ app/javascript/components/Squad.js
@@ -1,6 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+
 class Squad extends React.Component {
 
   constructor(props) {
