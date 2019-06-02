fb26ad506913d3f2d11fb9d7d1ed32c21e13c048
Steven Donald <weteamsteve@gmail.com>
2019-06-02 01:27:59 -0400

    Email test with debug

--- app/mailers/request_membership_mailer.rb
+++ app/mailers/request_membership_mailer.rb
@@ -8,9 +8,20 @@ class RequestMembershipMailer < ApplicationMailer
 
   # will pass in member so we know the user, squad and member data. amazing.
   def membership(member)
+    Rails.logger.debug("!!! Member inspect: #{member.inspect}")
+    #Rails.logger.debug("!!! Member.user inspect: #{member.user.inspect}")
+    #Rails.logger.debug("!!! Member.squad inspect: #{member.squad.inspect}")
 
-    @subject = "#{@member.user.name}'s membership for #{@member.squad.name} has been #{@member.membership}."
-    @body = "#{@member.user.name}'s membership for #{@member.squad.name} has been updated to #{@member.membership}."
+    @member = member
+
+    Rails.logger.debug("!!! @Member inspect: #{@member.inspect}")
+    @user = User.find(member.user_id)
+    @squad = Squad.find(member.squad_id)
+    Rails.logger.debug("!!! user inspect: #{@user.inspect}")
+    Rails.logger.debug("!!! squad inspect: #{@squad.inspect}")
+
+    @subject = "#{member.user.name}'s membership for #{@ember.squad.name} has been #{member.membership}."
+    @body = "#{member.user.name}'s membership for #{member.squad.name} has been updated to #{member.membership}."
 
     mail to: member.user.email,
          subject: @subject
