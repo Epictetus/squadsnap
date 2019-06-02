2b04988c167a67e66e29c49d1c819e97c8e3fd91
Steven Donald <weteamsteve@gmail.com>
2019-06-02 02:39:54 -0400

    Commented out authentication plain. Using app keys now on Google.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    authentication: "plain",
+    #authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
